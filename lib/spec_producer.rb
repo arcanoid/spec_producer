12684573761882fff5ee65e1a4902b588bd60d64
Vasilis Kalligas <billkall@gmail.com>
2016-10-15 21:50:22 +0300

    Clean up code that was factored out.

--- lib/spec_producer.rb
+++ lib/spec_producer.rb
@@ -80,62 +80,6 @@ module SpecProducer
   end
 
   def self.produce_factories
-    Dir.glob(Rails.root.join('app/models/*.rb')).each do |x|
-      require x
-    end
-    
-    not_valid_descendants = [ ActiveRecord::SchemaMigration, Delayed::Backend::ActiveRecord::Job ]
-
-    ActiveRecord::Base.descendants.reject { |descendant| not_valid_descendants.include? descendant }.each do |descendant|
-      final_text = "FactoryGirl.define do\n"
-      final_text << "  factory :#{descendant.name.underscore}, :class => #{descendant.name} do\n"
-
-      descendant.columns.each do |column|
-        value = case column.type
-                  when :string then "'#{descendant.name.underscore.upcase}_#{column.name.underscore.upcase}'"
-                  when :text then "'#{descendant.name.underscore.upcase}_#{column.name.underscore.upcase}'"
-                  when :integer then "#{rand(1...10)}"
-                  when :decimal then "#{rand(1.0...100.0)}"
-                  when :float then "#{rand(1.0...100.0)}"
-                  when :datetime then "'#{DateTime.now + 365}'"
-                  when :time then "'#{Time.now + 365*24*60*60}'"
-                  when :date then "'#{Date.today + 365}'"
-                  when :boolean then "#{rand(2) == 1 ? true : false}"
-                  when :binary then "#{5.times.collect { rand(0..1).to_s }.join('')}"
-                end
-
-        final_text << "    #{column.name} #{value}\n"
-      end
-
-      final_text << "  end\n"
-      final_text << "end"
-
-      if File.exists?(Rails.root.join("spec/factories/#{descendant.name.underscore}.rb"))
-        puts '#'*100
-        puts "Please, check whether the following lines are included in: spec/factories/" + descendant.name.underscore + ".rb\n"
-        puts '#'*100
-        puts "\n"
-        puts final_text
-      else
-        unless Dir.exists? Rails.root.join("spec")
-          puts "Generating spec directory"
-          Dir.mkdir(Rails.root.join("spec"))
-        end
-
-        unless Dir.exists? Rails.root.join("spec/factories")
-          puts "Generating spec/factories directory"
-          Dir.mkdir(Rails.root.join("spec/factories"))
-        end
-
-        path = "spec/factories/#{descendant.name.underscore}.rb"
-        puts "Producing factory file for: #{path}"
-        f = File.open("#{Rails.root.join(path)}", 'wb+')
-        f.write(final_text)
-        f.close
-      end
-    end
-
-    nil
     FactoriesProductionModule.produce_factories
   end
 
