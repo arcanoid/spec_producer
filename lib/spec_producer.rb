21086411d9fc8ebe68bf2a59097e613bacb64f03
Vasilis Kalligas <billkall@gmail.com>
2016-10-28 21:58:22 +0300

    Set proper category for specs to run.

--- lib/spec_producer.rb
+++ lib/spec_producer.rb
@@ -21,49 +21,49 @@ module SpecProducer
   def self.produce_specs_for_models
     SpecProductionModule.produce_specs_for_models
 
-    run_spec_tests
+    run_spec_tests 'models'
   end
 
   def self.produce_specs_for_routes
     SpecProductionModule.produce_specs_for_routes
 
-    run_spec_tests
+    run_spec_tests 'routes'
   end
 
   def self.produce_specs_for_views
     SpecProductionModule.produce_specs_for_views
 
-    run_spec_tests
+    run_spec_tests 'views'
   end
 
   def self.produce_specs_for_controllers
     SpecProductionModule.produce_specs_for_controllers
 
-    run_spec_tests
+    run_spec_tests 'controllers'
   end
 
   def self.produce_specs_for_helpers
     SpecProductionModule.produce_specs_for_helpers
 
-    run_spec_tests
+    run_spec_tests 'helpers'
   end
 
   def self.produce_specs_for_mailers
     SpecProductionModule.produce_specs_for_mailers
 
-    run_spec_tests
+    run_spec_tests 'mailers'
   end
 
   def self.produce_specs_for_jobs
     SpecProductionModule.produce_specs_for_jobs
 
-    run_spec_tests
+    run_spec_tests 'jobs'
   end
 
   def self.produce_specs_for_serializers
     SpecProductionModule.produce_specs_for_serializers
 
-    run_spec_tests
+    run_spec_tests 'serializers'
   end
 
   def self.set_up_necessities
@@ -117,9 +117,24 @@ module SpecProducer
     MissingFilesModule.print_missing_route_specs(options)
   end
 
-  def self.run_spec_tests
-    puts "\nRunning all current spec tests...\n"
+  def self.run_spec_tests(type = nil)
+    puts "\nRunning related spec tests...\n"
 
-    system 'bundle exec rake'
+    if type.nil?
+      system 'bundle exec rake'
+    else
+      command = case type
+                  when 'controllers' then 'bundle exec rspec spec/controllers'
+                  when 'views' then 'bundle exec rspec spec/views'
+                  when 'mailers' then 'bundle exec rspec spec/mailers'
+                  when 'jobs' then 'bundle exec rspec spec/jobs'
+                  when 'routes' then 'bundle exec rspec spec/routes'
+                  when 'helpers' then 'bundle exec rspec spec/helpers'
+                  when 'models' then 'bundle exec rspec spec/models'
+                  else 'bundle exec rspec'
+                end
+      
+      system command
+    end
   end
 end
