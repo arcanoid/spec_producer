cb09e824faebea9afb33c51e8fb8b47e25fb3a67
Vasilis Kalligas <billkall@gmail.com>
2016-12-27 22:32:56 +0200

    Initial take in new way of producing controller specs.

--- lib/spec_producer/producers/controllers_producer.rb
+++ lib/spec_producer/producers/controllers_producer.rb
@@ -6,10 +6,23 @@ module SpecProducer
       prepend Base
 
       def resources
-      	[]
+        (ApplicationController.descendants << ApplicationController).
+        reverse.
+        map { |desc| Resource.new(desc, desc.name, 'controller') }
       end
 
       def call(resource)
+        resource.obj.action_methods.each do |method_name|
+          builder.pending method_name
+        end
+      end
+
+      #######
+      private
+      #######
+
+      def require_helper_string
+        @require_helper_string ||= Utils::FileUtils.collect_helper_strings
       end
     end
   end
