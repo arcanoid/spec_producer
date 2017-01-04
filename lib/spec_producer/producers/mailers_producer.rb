d15f52177514d37354d6d33323713a073d2d64a3
Vasilis Kalligas <billkall@gmail.com>
2017-01-05 00:56:35 +0200

    First take for mailer spec producers.

--- lib/spec_producer/producers/mailers_producer.rb
+++ lib/spec_producer/producers/mailers_producer.rb
@@ -4,10 +4,20 @@ module SpecProducer
       prepend Base
 
       def resources
-      	[]
+        Dir["app/mailers/**/*.rb"].
+          map { |file| Resource.new(file, File.basename(file, ".rb").camelcase, 'mailer') }
       end
 
       def call(resource)
+        builder.pending 'mailer tests'
+      end
+
+      #######
+      private
+      #######
+
+      def require_helper_string
+        @require_helper_string ||= Utils::FileUtils.collect_helper_strings
       end
     end
   end
